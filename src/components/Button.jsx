aa532990b2f01310fea1a1c4a37eb32e34b42021
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 10:51:51 -0300

    base front

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,28 @@
 import { Link } from "react-router-dom";
 
-const Button = ({ label, href = "" }) => {
+const Button = ({ label, href = "", variant = "primary" }) => {
+  let styles = "";
+
+  switch (variant) {
+    case "primary":
+      styles =
+        "w-full bg-secondary-blue text-center transition text-light-white px-6 py-3 rounded hover:bg-hover-blue font-bold";
+      break;
+    case "secondary":
+      styles =
+        "w-full bg-white text-center border-2 transition border-transparent text-secondary-blue px-6 py-3 rounded hover:border-secondary-blue font-bold";
+      break;
+  }
+
   if (href) {
     return (
-      <Link
-        to={href}
-        className="w-full bg-secondary-blue text-center text-light-white px-4 py-2 rounded hover:bg-hover-blue"
-      >
+      <Link to={href} className={styles}>
         {label}
       </Link>
     );
   }
 
-  return (
-    <button className="w-full bg-secondary-blue text-center text-light-white py-2 rounded hover:bg-hover-blue">
-      {label}
-    </button>
-  );
+  return <button className={styles}>{label}</button>;
 };
 
 export default Button;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Button from "../components/Button";
 import BannerBg from "../imgs/banner-bg.png";
 import { Link } from "react-router-dom"; // Para navegação interna
 
@@ -15,33 +16,23 @@ const Home = () => {
           <h1 className="text-5xl font-bold text-light-white">
             Imersão Virtual na Fórmula E
           </h1>
-          <p className="text-xl text-light-white mt-4">
+          <p className="text-xl text-light-white mt-6 mb-4">
             Explore o futuro da mobilidade elétrica.
           </p>
-          <Link
-            to="/experiencia"
-            className="mt-6 inline-block bg-secondary-blue text-light-white px-6 py-3 font-bold rounded-lg"
-          >
-            Experimente Agora
-          </Link>
+          <Button label="Experimente Agora" href="/experiencia" />
         </div>
       </section>
 
       {/* Introdução à Fórmula E */}
       <section className="w-full max-w-screen-lg text-center px-6 md:px-0">
         <h2 className="text-3xl font-bold mb-4">O que é a Fórmula E?</h2>
-        <p className="text-lg leading-relaxed">
+        <p className="text-lg leading-relaxed mb-4">
           A Fórmula E é a primeira categoria de automobilismo totalmente
           elétrica, promovendo a sustentabilidade e inovação. Com corridas em
           circuitos urbanos ao redor do mundo, a Fórmula E busca acelerar a
           transição para tecnologias de energia limpa.
         </p>
-        <Link
-          to="/sobre"
-          className="mt-4 inline-block text-secondary-blue font-semibold"
-        >
-          Saiba mais
-        </Link>
+        <Button label="Saiba mais" href="/sobre" variant="secondary" />
       </section>
 
       {/* Problema */}
@@ -50,16 +41,11 @@ const Home = () => {
           <h2 className="text-3xl font-bold mb-6">
             Problemas a Serem Resolvidos
           </h2>
-          <p className="text-lg leading-relaxed mb-4">
+          <p className="text-lg leading-relaxed mb-8">
             A Fórmula E enfrenta desafios como baixa visibilidade, falta de
             engajamento entre os jovens e a experiência de evento limitada.
           </p>
-          <Link
-            to="/solucao"
-            className="mt-4 inline-block text-light-white font-semibold"
-          >
-            Veja como resolvemos isso
-          </Link>
+          <Button label="Veja como resolvemos isso" href="/solucao" />
         </div>
       </section>
 
@@ -83,12 +69,7 @@ const Home = () => {
         <h2 className="text-3xl font-bold text-center mb-6">Galeria</h2>
         {/* <GaleryPreview />  Componente que mostra algumas imagens da galeria */}
         <div className="text-center mt-6">
-          <Link
-            to="/galeria"
-            className="inline-block text-secondary-blue font-semibold"
-          >
-            Ver mais imagens
-          </Link>
+          <Button label="Ver mais imagens" href="/galeria" />
         </div>
       </section>
 
